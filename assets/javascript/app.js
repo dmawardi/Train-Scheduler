281be85e44a2832c0675b8e7ccedaedd835a0e93
Dannar Mawardi <dmawardi@gmail.com>
2019-07-26 23:33:13 +1000

    Firebase successfully written to/connected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,38 @@
 console.log('bacon connected');
 
 // Assign firebase database to variable
-var database = firebase.database();
+var database = firebase.database().ref('/Train-Scheduler');
 
-var trainName = [];
-var destination = [];
-var firstTrainTime = [];
+var trainNames = [];
+var destinationList = [];
+var firstTrainTimes = [];
 var frequency = [];
 
 // Takes user input and displays on board
-function inputSubmission(trainName, destination, firstMilitaryTime, frequencyInMinutes){
+function inputSubmission(train, destination, firstMilitaryTime, frequencyInMinutes){
+    trainNames.push(train);
+    destinationList.push(destination);
+    firstTrainTimes.push(firstMilitaryTime);
+    frequency.push(frequencyInMinutes);
 
+    // firebase update
+    database.set({
+        trainNames: trainNames,
+        destination: destinationList,
+        firstTrainTimes: firstTrainTimes,
+        frequency: frequency
+    })
 }
 
 
-database.ref().on('value', function(snap){
+database.on('value', function(snap){
     console.log(snap.val());
 
-    if (database.ref('/Train-Scheduler').exists()) {
-        trainName
+    if (snap.val() != null) {
+        trainNames = snap.val().trainName;
+        destinationList = snap.val().destination;
+        firstTrainTimes = snap.val().firstTrainTime;
+        frequency = snap.val().frequency;
     }
 
 }, function(error) {
